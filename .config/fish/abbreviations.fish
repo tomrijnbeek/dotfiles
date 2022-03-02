ca64b1711ee1b5fec9626e220c9be635ce96c24f
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-03-02 17:00:44 +0100

    🔡 Use abbreviations instead of aliases for fish

---
+++ .config/fish/abbreviations.fish
@@ -0,0 +1,12 @@
+# Folder navigation
+abbr -a up cd ..
+
+abbr -a ll ls -alF
+abbr -a la ls -A
+abbr -a l ls -CF
+
+# Python
+abbr -a py python3
+
+# Git
+abbr -a g git
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 source "$HOME/.config/fish/exports.fish"
-source "$HOME/.config/fish/aliases.fish"
+source "$HOME/.config/fish/abbreviations.fish"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
