70a25ac01202a703f846b94a87229438a353c515
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-03-02 17:37:01 +0100

    ✨ Add tmux config

--- create_links.fish
+++ create_links.fish
@@ -2,13 +2,14 @@ set -l basedir (dirname (readlink -m (status --current-filename)))
 
 function make_link
   echo "$argv[1] -> $argv[2]"
-  if find $argv[2]
+  if test -e $argv[2] || test -d $argv[2]
     mv $argv[2] $argv[2].bak
   end
   ln -sfn $argv[1] $argv[2]
 end
 
 make_link $basedir/.gitconfig ~/.gitconfig
+make_link $basedir/.tmux.conf ~/.tmux.conf
 
 for i in $basedir/.config/*
   make_link $i ~/.config/(basename $i)
