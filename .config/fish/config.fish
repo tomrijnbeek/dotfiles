28a46244f0ce620d30111227a2dfd842cd057dc4
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-03-04 14:22:27 +0100

    🐛 Remove wrong source from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/abbreviations.fish"
-source "$HOME/.config/fish/fisher.fish"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
