5a4c89885231f7cb484e36d42a93e3b5f97f76d1
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-03-04 14:36:55 +0100

    🐛 Add missing fisher conf.d imports

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/abbreviations.fish"
 
+for f in $fisher_path/conf.d/*
+  source $f
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
